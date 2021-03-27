a34acc90d2ffde8c46fcbe12d6aad50c10367e2b
rothexD <56934789+rothexD@users.noreply.github.com>
2021-03-27 21:12:01 +0100

    Helper that accesses Configuration
    Checks Configuration if Language and Version is supported.

---
+++ RestWebserver/RestWebservice-RemoteCompiling/Helpers/LanguageAndVersionHelper.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace RestWebservice_RemoteCompiling.Helpers
+{
+    public interface ILanguageAndVersionHelper
+    {
+        bool CheckConfigurationForVersionAndLanguage(string language, string version);
+        public List<string> GetSupportedLanguages();
+        public List<string> GetSupportedVersionsForLanguage(string language);
+    }
+    public class LanguageAndVersionHelper : ILanguageAndVersionHelper
+    {
+        private readonly IConfiguration _Configuration;
+
+        public LanguageAndVersionHelper(IConfiguration configuration)
+        {
+            _Configuration = configuration;
+        }
+
+        public bool CheckConfigurationForVersionAndLanguage(string language, string version)
+        {
+            bool isValidVersion = false;
+            var item = _Configuration.GetSection("supportedVersions").GetSection(language).GetChildren() ?? throw new NullReferenceException();
+            item.ToList().ForEach(x =>
+            {
+                if (x.Value == version)
+                {
+                    isValidVersion = true;
+                }
+            });
+            return isValidVersion;
+        }
+        public List<string> GetSupportedLanguages()
+        {
+            List<string> returnList = new List<string>();
+            var item = _Configuration.GetSection("supportedVersions").GetChildren().ToList();
+            item.ForEach(x => { returnList.Add(x.Key);Console.WriteLine(x); }) ;
+            return returnList;
+        }
+        public List<string> GetSupportedVersionsForLanguage(string language)
+        {
+            List<string> returnList = new List<string>();
+            var item = _Configuration.GetSection("supportedVersions").GetSection(language).GetChildren().ToList() ?? throw new NullReferenceException();
+            item.ForEach(x => returnList.Add(x.Value));
+            return returnList;
+        }
+    }
+}
