77ed649c614c8cd1f9da8b2d5e941ef9060ea513
rothexD <56934789+rothexD@users.noreply.github.com>
2021-03-27 21:11:15 +0100

    Json Body for codexecution

---
+++ RestWebserver/RestWebservice-RemoteCompiling/JsonObjClasses/JSON_Code.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace RestWebservice_RemoteCompiling.JsonObjClasses
+{
+    public class JSON_Code
+    {
+        [Required]
+        public string CodeAsValue { get; set; }
+        [Required]
+        public string CompileArguments { get; set; }
+    }
+}
