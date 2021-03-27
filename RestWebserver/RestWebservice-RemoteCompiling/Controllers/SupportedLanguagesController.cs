8a506f4f4ec325454005207ac0bb455745b179f8
rothexD <56934789+rothexD@users.noreply.github.com>
2021-03-27 21:13:17 +0100

    Help Controller
    Get Supported Lanuage
    Get Suppoerted Version for Language
    Get Licence information
    Get WhatAreWe
    Get GithubLink

---
+++ RestWebserver/RestWebservice-RemoteCompiling/Controllers/SupportedLanguagesController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RestWebservice_RemoteCompiling.JsonObjClasses;
+using System.Web;
+using System.Net.Http;
+using RestWebservice_RemoteCompiling.Helpers;
+
+namespace RestWebservice_RemoteCompiling.Controllers
+{
+    [Route("Api/Help")]
+    [ApiController]
+    public class SupportedLanguagesController : ControllerBase
+    {
+        private readonly ILanguageAndVersionHelper _LanguageAndVersionHelper;
+        public SupportedLanguagesController(ILanguageAndVersionHelper languageAndVersionHelper)
+        { 
+            _LanguageAndVersionHelper = languageAndVersionHelper;
+        }
+        [HttpGet("Languages")]
+        public IActionResult GetSupportedProgrammingLanguages()
+        {
+            try
+            {
+                return Ok(_LanguageAndVersionHelper.GetSupportedLanguages());
+            }
+            catch (NullReferenceException e)
+            {
+                return NotFound();
+            }
+        }
+        [HttpGet("{language}/SupportedVersions")]
+        public IActionResult GetSupportedVersionsForLanguage(string language)
+        {
+            try
+            {
+                return Ok(_LanguageAndVersionHelper.GetSupportedVersionsForLanguage(language));
+            }
+            catch (NullReferenceException e)
+            {
+                return NotFound();
+            }
+        }
+        [HttpGet("Licence")]
+        public IActionResult Licence()
+        {
+            try
+            {
+                return Ok("\"MIT License\"");
+            }
+            catch (NullReferenceException e)
+            {
+                return NotFound();
+            }
+        }
+        [HttpGet("PublicGithub")]
+        public IActionResult PublicGithub()
+        {
+            try
+            {
+                return Ok("\"https://github.com/kienboec/RemoteCompiling\"");
+            }
+            catch (NullReferenceException e)
+            {
+                return NotFound();
+            }
+        }
+        [HttpGet("WhatIsThisApi")]
+        public IActionResult WhatIsThisApi()
+        {
+            try
+            {
+                //change bla bla with actual names
+                return Ok("\"A student project from the University of Applied Sciences Vienna in the year 2021 (4th Semester). Developers: blabla under supervision of Daniel Kienboec. This API serves as a remote compiling service.\"");
+            }
+            catch (NullReferenceException e)
+            {
+                return NotFound();
+            }
+        }
+    }
+}
