78f2c7b16a38e0d398d8665ad88aee242e3bcd3a
rothexD <56934789+rothexD@users.noreply.github.com>
2021-03-27 21:09:47 +0100

    Code Validation and Execution Controller
    Place holder copy paste

---
+++ RestWebserver/RestWebservice-RemoteCompiling/Controllers/CompileController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RestWebservice_RemoteCompiling.JsonObjClasses;
+using System.Web;
+using System.Net.Http;
+using RestWebservice_RemoteCompiling.Helpers;
+
+namespace RestWebservice_RemoteCompiling.Controllers
+{
+
+    [ApiController]
+    [Route("/Api/Compile")]
+    public class CompileController : ControllerBase
+    {
+        private readonly ILanguageAndVersionHelper _LanguageAndVersionValidator;
+        private readonly HttpClient _Http;
+        public CompileController(HttpClient http, ILanguageAndVersionHelper languageAndVersionValidator)
+        {
+            _Http = http;
+            _LanguageAndVersionValidator = languageAndVersionValidator;
+        }
+        [HttpPost("{language}/{version}")]
+        public IActionResult ExecuteCodeWithVersion(string language, string version,JSON_Code Code)
+        {
+            try
+            {
+                if (!_LanguageAndVersionValidator.CheckConfigurationForVersionAndLanguage(language, version))
+                {
+                    return NotFound();
+                }
+                /*
+                * Start LXC container and run code              
+               */
+                return Ok(Code.CodeAsValue);            
+            }
+            catch(NullReferenceException e)
+            {
+                return NotFound();
+            }
+        }
+        
+    }
+}
---
+++ RestWebserver/RestWebservice-RemoteCompiling/Controllers/ValidationController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RestWebservice_RemoteCompiling.JsonObjClasses;
+using System.Web;
+using System.Net.Http;
+using RestWebservice_RemoteCompiling.Helpers;
+
+namespace RestWebservice_RemoteCompiling.Controllers
+{
+    [Route("Api/Validation")]
+    [ApiController]
+    public class ValidationController : ControllerBase
+    {
+
+            private readonly ILanguageAndVersionHelper _LanguageAndVersionValidator;
+            private readonly HttpClient _Http;
+            public ValidationController(HttpClient http, ILanguageAndVersionHelper languageAndVersionValidator)
+            {
+                _Http = http;
+                _LanguageAndVersionValidator = languageAndVersionValidator;
+            }
+            [HttpPost("{language}/{version}")]
+            public IActionResult ValidateCodeWithVersion(string language, string version, JSON_Code Code)
+            {
+                try
+                {
+                    if (!_LanguageAndVersionValidator.CheckConfigurationForVersionAndLanguage(language, version))
+                    {
+                        return NotFound();
+                    }
+                    /*
+                    * run validation here            
+                   */
+                    return Ok(Code.CodeAsValue);
+                }
+                catch (NullReferenceException e)
+                {
+                    return NotFound();
+                }
+            }
+
+        }
+    
+}
