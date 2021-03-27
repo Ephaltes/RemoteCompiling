b8ce03c10a702b2c2164929e26d838f8c14f5d91
rothexD <56934789+rothexD@users.noreply.github.com>
2021-03-27 21:10:39 +0100

    defined singletons and services
    httpclient -> needed in future
    language and version helper that accesses config

--- RestWebserver/RestWebservice-RemoteCompiling/Startup.cs
+++ RestWebserver/RestWebservice-RemoteCompiling/Startup.cs
@@ -11,6 +11,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using RestWebservice_RemoteCompiling.Helpers;
 
 namespace RestWebservice_RemoteCompiling
 {
@@ -26,6 +27,8 @@ namespace RestWebservice_RemoteCompiling
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHttpClient();
+            services.AddSingleton<ILanguageAndVersionHelper, LanguageAndVersionHelper>();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
