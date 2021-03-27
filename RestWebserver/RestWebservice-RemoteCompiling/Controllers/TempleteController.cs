9599b49bc95e774e530286376ba758c5e19b1134
rothexD <56934789+rothexD@users.noreply.github.com>
2021-03-27 21:12:23 +0100

    Template controller
    should return a template for the given programming language

---
+++ RestWebserver/RestWebservice-RemoteCompiling/Controllers/TempleteController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace RestWebservice_RemoteCompiling.Controllers
+{
+    [Route("Api/Templates")]
+    [ApiController]
+    public class TempleteController : ControllerBase
+    {
+        [HttpGet("{language}")]
+        public IActionResult GetTemplateForLanguage(string language)
+        {
+            if (!System.IO.File.Exists($"./Templates/{language.ToLower()}Template.json"))
+            {
+                return NotFound();
+            }
+            string jsonString = System.IO.File.ReadAllText($"./Templates/{language.ToLower()}Template.json");
+            return Ok(jsonString);
+        }
+    }
+}
