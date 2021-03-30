d3bd9e29120ff6dd59007533f189b4fbfd51a8ba
rothexD <56934789+rothexD@users.noreply.github.com>
2021-03-30 18:51:41 +0200

    removed endpoints and added and points
    removed supported languages and supported versions
    added supprted runtimes

--- RestWebserver/RestWebservice-RemoteCompiling/Controllers/HelpController.cs
+++ RestWebserver/RestWebservice-RemoteCompiling/Controllers/HelpController.cs
@@ -15,31 +15,19 @@ namespace RestWebservice_RemoteCompiling.Controllers
     [ApiController]
     public class HelpController : ControllerBase
     {
-        private readonly ILanguageAndVersionHelper _LanguageAndVersionHelper;
-        public HelpController(ILanguageAndVersionHelper languageAndVersionHelper)
+        private readonly IPistonHelper _PisonHelper;
+        public HelpController(IPistonHelper pistonHelper)
         { 
-            _LanguageAndVersionHelper = languageAndVersionHelper;
+            _PisonHelper = pistonHelper;
         }
-        [HttpGet("Languages")]
-        public IActionResult GetSupportedProgrammingLanguages()
+        [HttpGet("runtimes")]
+        public IActionResult GetSupportedRuntimes()
         {
             try
             {
-                return Ok(_LanguageAndVersionHelper.GetSupportedLanguages());
+                return Ok(_PisonHelper.GetSupportedRuntimes());
             }
-            catch (NullReferenceException e)
-            {
-                return NotFound();
-            }
-        }
-        [HttpGet("{language}/SupportedVersions")]
-        public IActionResult GetSupportedVersionsForLanguage(string language)
-        {
-            try
-            {
-                return Ok(_LanguageAndVersionHelper.GetSupportedVersionsForLanguage(language));
-            }
-            catch (NullReferenceException e)
+            catch
             {
                 return NotFound();
             }
@@ -47,39 +35,21 @@ namespace RestWebservice_RemoteCompiling.Controllers
         [HttpGet("Licence")]
         public IActionResult Licence()
         {
-            try
-            {
                 return Ok("\"MIT License\"");
-            }
-            catch (NullReferenceException e)
-            {
-                return NotFound();
-            }
         }
         [HttpGet("PublicGithub")]
         public IActionResult PublicGithub()
         {
-            try
-            {
                 return Ok("\"https://github.com/kienboec/RemoteCompiling\"");
-            }
-            catch (NullReferenceException e)
-            {
-                return NotFound();
-            }
+            
         }
         [HttpGet("WhatIsThisApi")]
         public IActionResult WhatIsThisApi()
         {
-            try
-            {
+
                 //change bla bla with actual names
                 return Ok("\"A student project from the University of Applied Sciences Vienna in the year 2021 (4th Semester). Developers: blabla under supervision of Daniel Kienboec. This API serves as a remote compiling service.\"");
-            }
-            catch (NullReferenceException e)
-            {
-                return NotFound();
-            }
+            
         }
     }
 }
