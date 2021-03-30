11f89a22e148918a6ae1d92c9c2c355abf77beec
rothexD <56934789+rothexD@users.noreply.github.com>
2021-03-30 18:52:26 +0200

    added new json obj

---
+++ RestWebserver/RestWebservice-RemoteCompiling/JsonObjClasses/JSON_sendCompileRequest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWebservice_RemoteCompiling.JsonObjClasses
+{
+    public class JSON_sendCompileRequest
+    {
+        public string language { get; set; }
+        public string version { get; set; }
+        public List<JSON_FileArray> files{ get; set; }
+        public string main { get; set; }
+        public string stdin { get; set; }
+        public List<string> args { get; set; }
+        public int compile_timeout { get; set; }
+        public int run_timeout { get; set; }
+   
+        public JSON_sendCompileRequest()
+    {
+            files = new List<JSON_FileArray>();
+            args = new List<string>();
+    }
+}
+
+}
---
+++ RestWebserver/RestWebservice-RemoteCompiling/JsonObjClasses/Piston/JSON_PistonReturn.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWebservice_RemoteCompiling.JsonObjClasses
+{
+    public class JSON_PistonReturn
+    {
+        public string stdout { get; set; }
+        public string stderr { get; set; }
+        public int code { get; set; }
+        public string signal { get; set; }
+    }
+}
