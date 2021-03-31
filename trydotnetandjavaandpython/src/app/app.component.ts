84d3f977464c63d251ebd7ad807ba603ec9695ab
Gurparkash Singh <gurparkashsingh99@gmail.com>
2021-03-31 12:52:02 +0200

    grid layout added to app.component

--- trydotnetandjavaandpython/src/app/app.component.ts
+++ trydotnetandjavaandpython/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CodeModel } from '@ngstack/code-editor';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'trydotnetandjavaandpython';
+  theme = 'vs-dark';
+
+  codeModel: CodeModel = {
+    language: 'csharp',
+    uri: 'Program.cs',
+    value: '',
+  };
+
+  options = {
+    contextmenu: true,
+    minimap: {
+      enabled: true,
+    },
+  };
+
+  onCodeChanged(value: any) {
+    console.log('CODE', value);
+  }
 }
