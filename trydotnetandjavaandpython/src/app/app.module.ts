bb5e355893cc9440c8647b4ba78d83af365b1b0c
Gurparkash Singh <gurparkashsingh99@gmail.com>
2021-03-31 12:52:02 +0200

    grid layout added to app.component

--- trydotnetandjavaandpython/src/app/app.module.ts
+++ trydotnetandjavaandpython/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { CodeEditorModule } from '@ngstack/code-editor';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -10,7 +12,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule, MatGridListModule, CodeEditorModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
