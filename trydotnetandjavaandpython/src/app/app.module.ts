4ef53e3c99c084b3400ac599819b528018954856
Gurparkash Singh <gurparkashsingh99@gmail.com>
2021-03-31 12:56:03 +0200

    mat icon added

--- trydotnetandjavaandpython/src/app/app.module.ts
+++ trydotnetandjavaandpython/src/app/app.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { CodeEditorModule } from '@ngstack/code-editor';
-import {MatTreeModule} from '@angular/material/tree';
+import { MatTreeModule } from '@angular/material/tree';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -13,7 +14,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule, MatGridListModule, MatTreeModule,
+    BrowserAnimationsModule, MatGridListModule, MatTreeModule, MatIconModule
     CodeEditorModule.forRoot()
   ],
   providers: [],
