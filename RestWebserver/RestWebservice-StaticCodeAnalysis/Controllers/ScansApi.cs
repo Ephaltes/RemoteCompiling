de4a652c344cdb8f6d8882d5735ad54c3202905c
expries <56935464+expries@users.noreply.github.com>
2021-11-01 22:24:58 +0100

    Added startup and controller for static code analysis api

---
+++ RestWebserver/RestWebservice-StaticCodeAnalysis/Controllers/ScansApi.cs
@@ -0,0 +1,178 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Authorization;
+using Swashbuckle.AspNetCore.Annotations;
+using RestWebservice_StaticCodeAnalysis.Security;
+using RestWebservice_StaticCodeAnalysis.DTOs;
+using Newtonsoft.Json;
+
+namespace RestWebservice_StaticCodeAnalysis.Controllers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [ApiController]
+    public class ScansApiController : ControllerBase
+    {
+        /// <summary>
+        /// Create a code analysis job
+        /// </summary>
+        /// <param name="body">Code to perform the analysis on</param>
+        /// <response code="200">A job that corresponds to the code analysis</response>
+        /// <response code="400">Not supported code-language or invalid code</response>
+        /// <response code="401">Token is missing or has expired</response>
+        [HttpPost]
+        [Route("/scans")]
+        [Authorize(AuthenticationSchemes = BearerAuthenticationHandler.SchemeName)]
+        [SwaggerOperation("CreateScanJob")]
+        [SwaggerResponse(statusCode: 200, type: typeof(ScanJobDto), description: "A job that corresponds to the code analysis")]
+        [SwaggerResponse(statusCode: 400, description: "Not supported code-language or invalid code")]
+        [SwaggerResponse(statusCode: 401, description: "Token is missing or has expired")]
+        public virtual IActionResult CreateScanJob([FromBody] CodeDto body)
+        {
+            //TODO: Uncomment the next line to return response 200 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
+            // return StatusCode(200, default(ScanJob));
+
+            //TODO: Uncomment the next line to return response 400 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
+            // return StatusCode(400);
+
+            //TODO: Uncomment the next line to return response 401 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
+            // return StatusCode(401);
+            string exampleJson = null;
+            exampleJson = "{\n  \"id\" : 1,\n  \"status\" : \"pending\"\n}";
+
+            var example = exampleJson != null
+            ? JsonConvert.DeserializeObject<ScanJobDto>(exampleJson)
+            : default(ScanJobDto);            //TODO: Change the data returned
+            return new ObjectResult(example);
+        }
+
+        /// <summary>
+        /// Delete a scan job and it's results
+        /// </summary>
+        /// <param name="scanId">Id of the scan to retrieve</param>
+        /// <response code="204">Scan job was deleted</response>
+        /// <response code="401">Token is missing or has expired or user is not permitted to see this scan job</response>
+        /// <response code="404">Scan with this id was not found</response>
+        [HttpDelete]
+        [Route("/scans/{scanId}")]
+        [Authorize(AuthenticationSchemes = BearerAuthenticationHandler.SchemeName)]
+        [SwaggerOperation("DeleteScanJob")]
+        [SwaggerResponse(statusCode: 204, description: "Scan job was deleted")]
+        [SwaggerResponse(statusCode: 401, description: "Token is missing or has expired or user is not permitted to see this scan job")]
+        [SwaggerResponse(statusCode: 404, description: "Scan with this id was not found")]
+        public virtual IActionResult DeleteScanJob([FromRoute][Required] string scanId)
+        {
+            //TODO: Uncomment the next line to return response 204 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
+            // return StatusCode(204);
+
+            //TODO: Uncomment the next line to return response 401 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
+            // return StatusCode(401);
+
+            //TODO: Uncomment the next line to return response 404 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
+            // return StatusCode(404);
+
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// See all of the current user's scan jobs
+        /// </summary>
+        /// <response code="200">List of all scan jobs</response>
+        /// <response code="401">Token is missing or has expired</response>
+        [HttpGet]
+        [Route("/scans")]
+        [Authorize(AuthenticationSchemes = BearerAuthenticationHandler.SchemeName)]
+        [SwaggerOperation("GetAllScanJobs")]
+        [SwaggerResponse(statusCode: 200, type: typeof(List<ScanJobDto>), description: "List of all scan jobs")]
+        [SwaggerResponse(statusCode: 401, description: "Token is missing or has expired")]
+        public virtual IActionResult GetAllScanJobs()
+        {
+            //TODO: Uncomment the next line to return response 200 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
+            // return StatusCode(200, default(List<ScanJob>));
+
+            //TODO: Uncomment the next line to return response 401 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
+            // return StatusCode(401);
+            string exampleJson = null;
+            exampleJson = "[ {\n  \"id\" : 1,\n  \"status\" : \"pending\"\n}, {\n  \"id\" : 1,\n  \"status\" : \"pending\"\n} ]";
+
+            var example = exampleJson != null
+            ? JsonConvert.DeserializeObject<List<ScanJobDto>>(exampleJson)
+            : default(List<ScanJobDto>);            //TODO: Change the data returned
+            return new ObjectResult(example);
+        }
+
+        /// <summary>
+        /// Retrieve a scan job's status
+        /// </summary>
+        /// <param name="scanId">Id of the scan to retrieve</param>
+        /// <response code="200">Results of the scan</response>
+        /// <response code="401">Token is missing or has expired or user is not permitted to see this scan job</response>
+        /// <response code="404">Scan with this id was not found</response>
+        [HttpGet]
+        [Route("/scans/{scanId}")]
+        [Authorize(AuthenticationSchemes = BearerAuthenticationHandler.SchemeName)]
+        [SwaggerOperation("GetScanJob")]
+        [SwaggerResponse(statusCode: 200, type: typeof(CodeDto), description: "Results of the scan")]
+        [SwaggerResponse(statusCode: 401, description: "Token is missing or has expired or user is not permitted to see this scan job")]
+        [SwaggerResponse(statusCode: 404, description: "Scan with this scanId was not found")]
+        public virtual IActionResult GetScanJob([FromRoute][Required] string scanId)
+        {
+            //TODO: Uncomment the next line to return response 200 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
+            // return StatusCode(200, default(Code));
+
+            //TODO: Uncomment the next line to return response 401 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
+            // return StatusCode(401);
+
+            //TODO: Uncomment the next line to return response 404 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
+            // return StatusCode(404);
+            string exampleJson = null;
+            exampleJson = "{\n  \"code-language\" : \"c#\",\n  \"code\" : \"code\"\n}";
+
+            var example = exampleJson != null
+            ? JsonConvert.DeserializeObject<CodeDto>(exampleJson)
+            : default(CodeDto);            //TODO: Change the data returned
+            return new ObjectResult(example);
+        }
+
+        /// <summary>
+        /// Retrieves a scan's results
+        /// </summary>
+        /// <param name="scanId">Id of the scan to retrieve</param>
+        /// <response code="200">Results of the scan</response>
+        /// <response code="401">Token is missing or has expired or user is not permitted to see this scan job</response>
+        /// <response code="404">Scan with this scanId was not found</response>
+        /// <response code="409">Scan job does not yet have status 'available'</response>
+        [HttpGet]
+        [Route("/scans/{scanId}/results")]
+        [Authorize(AuthenticationSchemes = BearerAuthenticationHandler.SchemeName)]
+        [SwaggerOperation("GetScanResults")]
+        [SwaggerResponse(statusCode: 200, type: typeof(ScanDto), description: "Results of the scan")]
+        [SwaggerResponse(statusCode: 401, description: "Token is missing or has expired or user is not permitted to see this scan job")]
+        [SwaggerResponse(statusCode: 404, description: "Scan with this scanId was not found")]
+        [SwaggerResponse(statusCode: 409, description: "Scan job does not yet have status 'available'")]
+        public virtual IActionResult GetScanResults([FromRoute][Required] string scanId)
+        {
+            //TODO: Uncomment the next line to return response 200 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
+            // return StatusCode(200, default(Scan));
+
+            //TODO: Uncomment the next line to return response 401 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
+            // return StatusCode(401);
+
+            //TODO: Uncomment the next line to return response 404 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
+            // return StatusCode(404);
+
+            //TODO: Uncomment the next line to return response 409 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
+            // return StatusCode(409);
+            string exampleJson = null;
+            exampleJson = "{\n  \"total\" : 0,\n  \"issues\" : [ {\n    \"severity\" : \"minor\",\n    \"textLocation\" : {\n      \"endLine\" : 1,\n      \"endOffset\" : 1,\n      \"startOffset\" : 6,\n      \"startLine\" : 1\n    },\n    \"component\" : \"sample-project:Program.cs\",\n    \"type\" : \"CodeSmell\"\n  }, {\n    \"severity\" : \"minor\",\n    \"textLocation\" : {\n      \"endLine\" : 1,\n      \"endOffset\" : 1,\n      \"startOffset\" : 6,\n      \"startLine\" : 1\n    },\n    \"component\" : \"sample-project:Program.cs\",\n    \"type\" : \"CodeSmell\"\n  } ]\n}";
+
+            var example = exampleJson != null
+            ? JsonConvert.DeserializeObject<ScanDto>(exampleJson)
+            : default(ScanDto);            //TODO: Change the data returned
+            return new ObjectResult(example);
+        }
+    }
+}
--- RestWebserver/RestWebservice-StaticCodeAnalysis/Startup.cs
+++ RestWebserver/RestWebservice-StaticCodeAnalysis/Startup.cs
@@ -1,55 +1,110 @@
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+using RestWebservice_StaticCodeAnalysis.Security;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace RestWebservice_StaticCodeAnalysis
 {
+    /// <summary>
+    /// Startup class for API configuration
+    /// </summary>
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
+        /// <summary>
+        /// Configuration
+        /// </summary>
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        private readonly IWebHostEnvironment _hostingEnv;
+
+        /// <summary>
+        /// Startup constructor
+        /// </summary>
+        /// <param name="configuration"></param>
+        /// <param name="env"></param>
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
-            services.AddRazorPages();
+            Configuration = configuration;
+            _hostingEnv = env;
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to add services to the container.
+        /// </summary>
+        /// <param name="services"></param>
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+                .AddMvc(options =>
+                {
+                    options.InputFormatters.RemoveType<Microsoft.AspNetCore.Mvc.Formatters.SystemTextJsonInputFormatter>();
+                    options.OutputFormatters.RemoveType<Microsoft.AspNetCore.Mvc.Formatters.SystemTextJsonOutputFormatter>();
+                })
+                .AddNewtonsoftJson(opts =>
+                {
+                    opts.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                    opts.SerializerSettings.Converters.Add(new StringEnumConverter(new CamelCaseNamingStrategy()));
+                })
+                .AddXmlSerializerFormatters();
+
+            services.AddAuthentication(BearerAuthenticationHandler.SchemeName)
+                .AddScheme<AuthenticationSchemeOptions, BearerAuthenticationHandler>(BearerAuthenticationHandler.SchemeName, null);
+
+            services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+                {
+                    c.SwaggerDoc("1.0.0", new OpenApiInfo
+                    {
+                        Version = "1.0.0",
+                        Title = "Static Code Analysis API",
+                        Description = "Static Code Analysis API for the Remote Compiling project",
+                        Contact = new OpenApiContact()
+                        {
+                            Name = "Swagger Codegen Contributors",
+                            Url = new Uri("https://github.com/swagger-api/swagger-codegen")
+                        }
+                    });
+                    c.CustomSchemaIds(type => type.FullName);
+                    c.IncludeXmlComments($"{AppContext.BaseDirectory}{Path.DirectorySeparatorChar}{_hostingEnv.ApplicationName}.xml");
+                });
+        }
+
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="env"></param>
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
 
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
 
             app.UseRouting();
 
             app.UseAuthorization();
 
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/1.0.0/swagger.json", "Static Code Analysis API");
+            });
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapControllers();
             });
         }
     }
