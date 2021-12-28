5ac78a1f7e3b9f20359d147cb59f0d61010d320c
Gurparkash Singh <if20b212@technikum-wien.at>
2021-12-28 13:23:28 +0100

    create new project with exercise list

--- trydotnetandjavaandpython/src/app/add-new-folder/add-new-folder.component.ts
+++ trydotnetandjavaandpython/src/app/add-new-folder/add-new-folder.component.ts
@@ -1,13 +1,16 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ExerciseNode } from '../exercise-module/exercise-node';
 import { FileNodeType } from '../file-module/file-node';
 import { forbiddenEndingValidator, forbiddenNameValidator } from '../forbidden-name.directive';
+import { ExerciseService } from '../service/exercise.service';
 
 @Component({
   selector: 'app-add-new-folder',
   templateUrl: './add-new-folder.component.html',
-  styleUrls: ['./add-new-folder.component.scss']
+  styleUrls: ['./add-new-folder.component.scss'],
+  providers: [ExerciseService]
 })
 
 export class AddNewFolderComponent implements OnInit {
@@ -26,9 +29,11 @@ export class AddNewFolderComponent implements OnInit {
         }
     }` },
   ];
+  exercises: ExerciseNode[] = []
   emittingData: { name: string, language: FileNodeType }
   validData: boolean;
-  constructor(private fb: FormBuilder, @Inject(MAT_DIALOG_DATA) public fileList: string[]) {
+  constructor(private fb: FormBuilder, @Inject(MAT_DIALOG_DATA) public fileList: string[], public exerciseService: ExerciseService) {
+    exerciseService.getExercises().subscribe(res => this.exercises = res.data);
     this.validData = false;
     this.emittingData = { name: "", language: FileNodeType.folder };
     this.newFileForm = fb.group({
