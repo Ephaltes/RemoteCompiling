bc13385db9b0220171a7903130f00d39390c4e7f
Gurparkash Singh <if20b212@technikum-wien.at>
2021-12-30 13:06:50 +0100

    removed not used

--- trydotnetandjavaandpython/src/app/code-meta/codeLanguageMeta.ts
+++
@@ -1,7 +0,0 @@
-export const languages = [
-    {name:"csharp", version:"5.0.201"},
-    {name:"java", version:"15.0.2"},
-    {name:"python", version:"3.9.1"},
-    {name:"gcc", version:"10.2.0"},
-]
-export const languageAmount = languages.length; 
